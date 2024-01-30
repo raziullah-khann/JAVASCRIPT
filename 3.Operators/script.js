c188206037b86b353fbac31f5adc607264634169
raziullah-khann <raziullahkhan25@gmail.com>
2024-01-30 23:02:02 +0530

    Working with Operators

---
+++ 3.Operators/script.js
@@ -0,0 +1,37 @@
+// OPERATORS 
+// 1.ARITHMETIC OPERATOR
+let num1 = 2;
+let num2 = 4;
+// console.log('The result of num1 + num2 is ',(num1+num2));
+// console.log('The result of num1 - num2 is ',(num1-num2));
+// console.log('The result of num1 * num2 is ',(num1*num2));
+// console.log('The result of num1 / num2 is ',(num1/num2));
+// console.log('The result of num1 % num2 is ',(num1%num2));
+// console.log('The result of num1 ** num2 is ',(num1**num2));
+// console.log('The result of num1 and num2 is ',(++num1));
+
+//ASSIGNMENT OPERATOR
+// console.log('The result of num1 += num2 is ',(num1+=num2));
+// console.log('The result of num1 -= num2 is ',(num1-=num2));
+// console.log('The result of num1 *= num2 is ',(num1*=num2));
+// console.log('The result of num1 /= num2 is ',(num1/=num2));
+// console.log('The result of num1 %= num2 is ',(num1%=num2));
+// console.log('The result of num1 **= num2 is ',(num1**=num2));
+
+//COMPARISON OPERATOR
+// console.log('The result of num1 == num2 is ',(num1==num2));
+// console.log('The result of num1 != num2 is ',(num1!=num2));
+// console.log('The result of num1 === num2 is ',(num1===num2));
+// console.log('The result of num1 !== num2 is ',(num1!==num2));
+// console.log('The result of num1 > num2 is ',(num1>num2));
+// console.log('The result of num1 >= num2 is ',(num1>=num2));
+// console.log('The result of num1 < num2 is ',(num1<num2));
+// console.log('The result of num1 <= num2 is ',(num1<=num2));
+
+//LOGICAL OPERATOR
+// const first = true;
+// const second = true;
+// const third = true;
+// console.log('The result of first && second is ',(first&&second));
+// console.log('The result of first || second is ',(first||second));
+// console.log('The result of third !  ',(!third));
