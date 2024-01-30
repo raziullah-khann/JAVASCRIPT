f06f92715490e7f38dcd470b30aa7ffce022e72c
raziullah-khann <raziullahkhan25@gmail.com>
2024-01-30 23:06:51 +0530

    Working with switch statement

---
+++ 5.Switch Statements/script.js
@@ -0,0 +1,21 @@
+const Grade = "H";
+switch(Grade){
+    case "A":
+        console.log('A-grade');
+        break;
+
+    case "B":
+        console.log('B-grade');
+        break;
+
+    case "C":
+        console.log('C-grade');
+        break;
+
+    case "D":
+        console.log('D-grade');
+        break;
+        
+    default:
+        console.log('Invalid Input');    
+}
